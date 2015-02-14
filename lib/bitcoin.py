88cb62f2cbc563304f6148f5a655fc113c89ce1b
ELMr4Ever <Denis@iMac.local>
2015-02-14 01:18:00 -0800

    oops

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -674,7 +674,7 @@ DARKCOIN_HEADERS = (DARKCOIN_HEADER_PUB, DARKCOIN_HEADER_PRIV)
 TESTNET_HEADERS = (TESTNET_HEADER_PUB, TESTNET_HEADER_PRIV)
 
 def _get_headers(testnet):
-    """Returns the correct headers for either testnet or bitcoin, in the form
+    """Returns the correct headers for either testnet or darkcoin, in the form
     of a 2-tuple, like (public, private)."""
     if testnet:
         return TESTNET_HEADERS
@@ -733,7 +733,7 @@ def xpub_from_xprv(xprv, testnet=False):
 def bip32_root(seed, testnet=False):
     import hmac
     header_pub, header_priv = _get_headers(testnet)
-    I = hmac.new("Bitcoin seed", seed, hashlib.sha512).digest()
+    I = hmac.new("Darkcoin seed", seed, hashlib.sha512).digest()
     master_k = I[0:32]
     master_c = I[32:]
     K, cK = get_pubkeys_from_secret(master_k)
